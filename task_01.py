ac485967e96645c0fbb50d9540d690e7c77474e8
Sam Cohen-DeVries <samuel.cohen-devries@spsmail.cuny.edu>
2014-09-24 06:25:48 -0400

    Updated Task 1

--- task_01.py
+++ task_01.py
@@ -2,25 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import data
-SHAKE1 = data.SHAKESPEARE.split("\n")
+TOTAL_WORDS = 0.00
+NUM_LINES = 0.00
 MAXIMUM_WORDS = 0
-for line in SHAKE1:
-    y = (line).split(" ")
-    if len(y) > MAXIMUM_WORDS:
-        MAXIMUM_WORDS = len(y)
-print "\n *** max number of words in a line = " + str(MAXIMUM_WORDS)
+MINIMUM_WORDS = 0
+for LINE in data.SHAKESPEARE.split("\n"):
+    NUM_LINES += 1.00
+    WORDS = LINE.split()
+    WORD_CT = len(WORDS)
+    if WORD_CT < MINIMUM_WORDS or MINIMUM_WORDS == 0:
+        MINIMUM_WORDS = WORD_CT
+    if WORD_CT > MAXIMUM_WORDS:
+        MAXIMUM_WORDS = WORD_CT
+    NUM_CRISPIAN = data.SHAKESPEARE.count("Crispian")
+    TOTAL_WORDS += len(WORDS)
+AVERAGE_WORDS = TOTAL_WORDS / NUM_LINES
 
-import data
-SHAKE1 = data.SHAKESPEARE.split("\n")
-MINIMUM_WORDS = MAXIMUM_WORDS
-for line in SHAKE1:
-    y = (line).split(" ")
-    if len(y) < MINIMUM_WORDS:
-        MINIMUM_WORDS = len(y)
+
 print "\n *** min number of words in a line = " + str(MINIMUM_WORDS)
+print "\n *** max number of words in a line = " + str(MAXIMUM_WORDS)
+print "\n *** Amt of times the word 'Crispian' occurs = " + str(NUM_CRISPIAN)
+print "\n *** Avg words per line = " + str(AVERAGE_WORDS)
 
-SHAKE2 = data.SHAKESPEARE.split()
-AVERAGE_WORDS = float(len(SHAKE2) / len(SHAKE1))
-
-NUM_CRISPIAN = data.SHAKESPEARE.count("Crispian")
-print NUM_CRISPIAN
