5285f0749c60b2b48a88bc937212980c32cc1710
Sam Cohen-DeVries <samuel.cohen-devries@spsmail.cuny.edu>
2014-09-29 03:20:02 -0400

    Task 05

--- task_05.py
+++ task_05.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import data
-MATCHUPS = ""
+MATCHUPS = []
 COUNTER = 0
 for ROW_INDEX, ROW_NAME in enumerate(data.VERSUS):
     for COLUMN_INDEX, COLUMN_NAME in enumerate(data.VERSUS):
         if ROW_INDEX > COLUMN_INDEX:
             COUNTER += 1
             MATCHUPS.append(COUNTER, "{0}", "{1}".format(ROW_NAME, COLUMN_NAME)
-print MATCHUPS.strip()
+    print MATCHUPS
