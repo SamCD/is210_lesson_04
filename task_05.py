2e824325aaf6be2b8c90806913ea7b8748ec5a35
Sam Cohen-DeVries <samuel.cohen-devries@spsmail.cuny.edu>
2014-10-02 07:06:48 -0400

    Task 05

--- task_05.py
+++ task_05.py
@@ -10,7 +10,7 @@ for ROW_INDEX, ROW_NAME in enumerate(data.VERSUS):
         if ROW_INDEX > COLUMN_INDEX:
             COUNTER += 1
             MATCH = '{COUNTER},"{0}","{1}"'.format(ROW_NAME,
-                COLUMN_NAME, COUNTER=COUNTER)
+                                                    COLUMN_NAME, COUNTER=COUNTER)
             MATCHUPS.append(MATCH)
 MATCHUPS = '\n'.join(MATCHUPS)
 print MATCHUPS
